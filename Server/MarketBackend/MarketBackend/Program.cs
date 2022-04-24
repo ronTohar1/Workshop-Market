41a6000721e2b08bfb121a878e25a16b0355ca62
Dave4321 <davidsin@post.bgu.ac.il>
2022-04-24 23:04:43 +0300

    set up Nlog example usage

--- Server/MarketBackend/MarketBackend/Program.cs
+++ Server/MarketBackend/MarketBackend/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MarketBackend.BusinessLayer.Buyers.Guests;
 using System;
-
+using NLog;
+using NLog.Config;
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
@@ -13,6 +14,39 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Console.WriteLine($"the number 2 is even");
             else
                 Console.WriteLine($"the number 2 is not even");
+            
+            log_example();
+        }
+        static void log_example()
+        {
+            //---------------- log setup: done only once when the system comes up --------------
+            var path = Directory.GetCurrentDirectory();
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile1 = new NLog.Targets.FileTarget("logfile1") { FileName = Path.Combine(path, "logs", "event_logs.txt") };
+            var logfile2 = new NLog.Targets.FileTarget("logfile2") { FileName = Path.Combine(path, "logs", "error_logs.txt") };
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Info, logfile1);
+            config.AddRule(LogLevel.Error, LogLevel.Error, logfile2);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+            //---------------------------------end of log setup --------------------------------
+
+            var logger = NLog.LogManager.GetCurrentClassLogger();//thread safe
+            try
+            {
+                logger.Info($"Guest with id: {123} purchased a {"banana"} !");
+                throw new Exception("BOOM!!!");
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Guest with id: {123} doesn't have the right permission");
+            }
+
         }
     }
 }

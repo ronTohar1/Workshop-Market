e9b3864630a692864f839553671aae8d3dcb7e96
Dave4321 <davidsin@post.bgu.ac.il>
2022-04-25 00:07:07 +0300

    logger
    added a systemlogger, an example for usage is in the Program.cs file

--- Server/MarketBackend/MarketBackend/Program.cs
+++ Server/MarketBackend/MarketBackend/Program.cs
@@ -1,7 +1,7 @@
 ﻿using MarketBackend.BusinessLayer.Buyers.Guests;
 using System;
 using NLog;
-using NLog.Config;
+using SystemLog;
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
@@ -19,24 +19,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
         static void log_example()
         {
-            //---------------- log setup: done only once when the system comes up --------------
-            var path = Directory.GetCurrentDirectory();
 
-            var config = new NLog.Config.LoggingConfiguration();
-
-            // Targets where to log to: File and Console
-            var logfile1 = new NLog.Targets.FileTarget("logfile1") { FileName = Path.Combine(path, "logs", "event_logs.txt") };
-            var logfile2 = new NLog.Targets.FileTarget("logfile2") { FileName = Path.Combine(path, "logs", "error_logs.txt") };
-
-            // Rules for mapping loggers to targets            
-            config.AddRule(LogLevel.Info, LogLevel.Info, logfile1);
-            config.AddRule(LogLevel.Error, LogLevel.Error, logfile2);
-
-            // Apply config           
-            NLog.LogManager.Configuration = config;
-            //---------------------------------end of log setup --------------------------------
-
-            var logger = NLog.LogManager.GetCurrentClassLogger();//thread safe
+            Logger logger = SystemLogger.getLogger();
             try
             {
                 logger.Info($"Guest with id: {123} purchased a {"banana"} !");
---
+++ Server/MarketBackend/MarketBackend/SystemLog/SystemLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+namespace SystemLog
+{
+    class SystemLogger
+    {
+        private static Logger _logger ;
+        public static Logger getLogger()
+        {
+            if (_logger == null)
+            {
+                //---------------- log setup: done only once when the system comes up --------------
+                string currentPath = Directory.GetCurrentDirectory();
+                string new_path = Path.Combine(currentPath, @"..\..\..\", "SystemLog");
+
+
+                var config = new NLog.Config.LoggingConfiguration();
+
+                // Targets where to log to: File and Console
+                var logfile1 = new NLog.Targets.FileTarget("logfile1") { FileName = Path.Combine(new_path, "event_logs.txt") };
+                var logfile2 = new NLog.Targets.FileTarget("logfile2") { FileName = Path.Combine(new_path, "error_logs.txt") };
+
+                // Rules for mapping loggers to targets            
+                config.AddRule(LogLevel.Info, LogLevel.Info, logfile1);
+                config.AddRule(LogLevel.Error, LogLevel.Error, logfile2);
+
+                // Apply config           
+                NLog.LogManager.Configuration = config;
+                //---------------------------------end of log setup --------------------------------
+                _logger = NLog.LogManager.GetCurrentClassLogger();
+            }
+            return _logger;
+        }
+
+    }
+}

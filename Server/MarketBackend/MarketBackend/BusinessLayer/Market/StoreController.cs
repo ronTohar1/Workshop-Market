835347058c77f907f033917591017f0c586afccc
AmitZivan <amitziv@post.bgu.ac.il>
2022-04-25 21:34:41 +0300

    Started implementing StoreController
    Added fields, implemented a constructor and added skeleton for methods, without comments of the related requirements yet

--- Server/MarketBackend/MarketBackend/BusinessLayer/Market/StoreController.cs
+++ Server/MarketBackend/MarketBackend/BusinessLayer/Market/StoreController.cs
@@ -2,7 +2,38 @@
 
 public class StoreController
 {
-	public StoreController()
+	private Dictionary<int, Store> openStores;
+	private Dictionary<int, Store> closedStores; 
+
+	private MembersController membersController;
+
+	private static int storeIdCounter = 0; // the next store id
+
+	// creates a new StoreController without stores yet
+	public StoreController(MembersController membersController)
 	{
+		this.membersController = membersController;
+
+		this.openStores = new Dictionary<int, Store>(); 
+		this.closedStores = new Dictionary<int, Store>();
 	}
+
+	public Store GetStore(int storeId)
+    {
+		return null;
+    }
+
+	public int GetStoreIdByName(string storeName)
+    {
+		// The stores in the system have a unique name for simplicity todo: check when adding a store
+		return -1; 
+	}
+
+	// opens a new store and returns its id
+	public int OpenNewStore(int memberId, string storeName)
+    {
+		return -1; 
+    }
+
+
 }

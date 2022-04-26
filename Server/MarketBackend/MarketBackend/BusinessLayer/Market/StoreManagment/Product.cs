d822a65567d3a856f4b9d23f41ec72ffce6729a6
Dave4321 <davidsin@post.bgu.ac.il>
2022-04-26 21:10:00 +0300

    market classes tests
    stoped with implementation and start to test

--- Server/MarketBackend/MarketBackend/BusinessLayer/Market/StoreManagment/Product.cs
+++ Server/MarketBackend/MarketBackend/BusinessLayer/Market/StoreManagment/Product.cs
@@ -1,28 +1,43 @@
 ﻿using System;
 namespace MarketBackend.BusinessLayer.Market.StoreManagment
 { 
-public class Product
-{
-	private string name;
-	private Dictionary<string, double> discounts; //mapping between discount code and discount percentage 
-	private int amountInInventory;
-	public Product(string product_name)
+	public class Product
 	{
-		name = product_name;
-		discounts = new Dictionary<string, double>();
-		amountInInventory = 0;
+		private string name;
+		private int amountInInventory;
+		private Dictionary<string, double> discounts; //mapping between discount code and discount percentage 
+		private List<PurchaseOption> purchaseOptions;
+		
+		public Product(string product_name)
+		{
+			name = product_name;
+			amountInInventory = 0;
+			discounts = new Dictionary<string, double>();
+			purchaseOptions = new List<PurchaseOption>();
+		}
+		public void AddNewDiscount(string discount_code, double discount_percentage) { 
+			discounts[discount_code] = discount_percentage;
+		}
+		public void AddToInventory(int amountToAdd) { 
+			amountInInventory = amountInInventory + amountToAdd;
+		} 
+		public void RemoveFromInventory(int amountToRemove) { 
+			if (amountInInventory<amountToRemove)
+				throw new StoreManagmentException($"Not enough products of {name} in storage");
+			else
+				amountInInventory = amountInInventory - amountToRemove;
+		}
+		public void AddPurchaseOption(PurchaseOption purchaseOption) {
+			if (!purchaseOptions.Contains(purchaseOption))	
+				purchaseOptions.Add(purchaseOption);
+		}
+		public void RemovePurchaseOption(PurchaseOption purchaseOption)
+		{
+			if (!purchaseOptions.Contains(purchaseOption))
+				throw new StoreManagmentException($"Not enough products of {name} in storage");
+			else
+				purchaseOptions.Remove(purchaseOption);
+		}
+
 	}
-	public void addNewDiscount(string discount_code, double discount_percentage) { 
-		discounts[discount_code] = discount_percentage;
-	}
-	public void addToInventory(int amountToAdd) { 
-		amountInInventory = amountInInventory + amountToAdd;
-	} 
-	public void removeFromInventory(int amountToRemove) { 
-		if (amountInInventory<amountToRemove)
-			throw new StoreManagmentException($"Not enough products of {name} in storage");
-		else
-			amountInInventory = amountInInventory - amountToRemove;
-	} 
-}
 }

1625321dbd3ca0b3ccf154f9d5e336883a2c9356
Idan-Bandoil <idanban@post.bgu.ac.il>
2022-04-27 14:44:14 +0300

    Service Layer Setup
    Added classes and method signatures of the service layer

---
+++ Server/MarketBackend/MarketBackend/ServiceLayer/AdminFacade.cs
@@ -0,0 +1,20 @@
+﻿using MarketBackend.BusinessLayer.Market.StoreManagment;
+using MarketBackend.ServiceLayer.ServiceDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketBackend.ServiceLayer
+{
+    internal class AdminFacade
+    {
+        
+        public Response<IList<Purchase>> GetPurchaseHistory(int currUserId, int storeId)
+        {
+            return new Response<IList<Purchase>>();
+        }
+
+    }
+}
---
+++ Server/MarketBackend/MarketBackend/ServiceLayer/SystemFacade.cs
@@ -0,0 +1,54 @@
+﻿using MarketBackend.ServiceLayer.ServiceDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketBackend.ServiceLayer
+{
+    internal class SystemFacade
+    {
+        public SystemFacade()
+        {
+
+        }
+
+        public Response<bool> OpenMarket()
+        {
+            return new Response<bool>();
+        }
+
+        public Response<int> AddNewSupplier(string supplierName)
+        {
+            return new Response<int>();
+        }
+
+        public Response<bool> RemoveSupplier(string supplierName)
+        {
+            return new Response<bool>();
+        }
+
+        public Response<bool> SerSupplierInfo(string supplierName)
+        {
+            return new Response<bool>();
+        }
+
+        public Response<int> AddNewPaymentService(string paymentServiceName)
+        {
+            return new Response<int>();
+        }
+
+        public Response<bool> RemovePaymentService(string paymentServiceName)
+        {
+            return new Response<bool>();
+        }
+
+        public Response<bool> SetPaymentServiceInfo(string paymentServiceName)
+        {
+            return new Response<bool>();
+        }
+
+
+    }
+}

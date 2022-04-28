6894ac631d8d4c4c19dc78fd952fdd5f6cb29c60
Roi Kant <kant@post.bgu.ac.il>
2022-04-28 21:27:12 +0300

    added external system
    added a beginner implementation of the external system to support a fully running system without the existence of actual external systems yet

---
+++ Server/MarketBackend/MarketBackend/BusinessLayer/System/ExternalServicesController.cs
@@ -0,0 +1,33 @@
+﻿using MarketBackend.BusinessLayer.Market.StoreManagment;
+using System;
+using System.Collections.Concurrent;
+
+public class ExternalServicesController
+{
+	private IDictionary<int, IExternalPaymentSystem> paymentsSystems;
+	private IDictionary<int, IExternalSupplySystem> supplySystems;
+	public ExternalServicesController()
+	{
+		paymentsSystems = new ConcurrentDictionary<int, IExternalPaymentSystem>();
+		supplySystems = new ConcurrentDictionary<int, IExternalSupplySystem>();
+	}
+
+	public bool makePayment(int id)
+    {
+		if (!paymentsSystems.ContainsKey(id))
+        {
+			throw new MarketException($"No payment system with id {id} in the external payment systems");
+		}
+		return paymentsSystems[id].makePayment();
+    }
+
+	public bool makeDelivery(int id)
+    {
+		if (!supplySystems.ContainsKey(id))
+        {
+			throw new MarketException($"No supply system with id {id} in the external supply systems");
+		}
+		return supplySystems[id].supplyDelivery();
+	}
+
+}

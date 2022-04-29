ebe0b7beb171d09adcc2fa0f895304a2c6bfec5a
Idan-Bandoil <idanban@post.bgu.ac.il>
2022-04-29 15:59:29 +0300

    Merged Guest and Member facedes into Buyer facade

--- Server/MarketBackend/MarketBackend/ServiceLayer/BuyerFacade.cs
+++ Server/MarketBackend/MarketBackend/ServiceLayer/BuyerFacade.cs
@@ -29,5 +29,49 @@ namespace MarketBackend.ServiceLayer
             return new Response<bool>();
         }
 
+        public Response<int> Enter()
+        {
+            return new Response<int>();
+        }
+
+        public Response<bool> Leave()
+        {
+            return new Response<bool>();
+        }
+        public Response<ServiceStore> GetStoreInfo(int storeId)
+        {
+            return new Response<ServiceStore>();
+        }
+
+        public Response<ServiceStore> GetStoreInfo(string storeName)
+        {
+            return new Response<ServiceStore>();
+        }
+
+        public Response<IList<ServiceProduct>> ProductsSearch(ProductFilter filter, int numberToLoad, int fromIndex)
+        {
+            return new Response<IList<ServiceProduct>>();
+        }
+
+        public Response<int> Register(string userName, string password)
+        {
+            return new Response<int>();
+        }
+
+        public Response<int> Login(string userName, string password)
+        {
+            return new Response<int>();
+        }
+
+        public Response<bool> Logout(int memberId)
+        {
+            return new Response<bool>();
+        }
+
+        public Response<bool> AddProductReview(int memberId, int storeId, int productId, string review)
+        {
+            return new Response<bool>();
+        }
+
     }
 }
---
+++ Server/MarketBackend/MarketBackened.Tests/Acceptance/GuestReqsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MarketBackend.
+
+namespace MarketBackened.Tests.Acceptance
+{
+    public class GuestReqsTests : IDisposable
+    {
+        private BuyerFacade buyerFacade;
+
+        public GuestReqsTests()
+        {
+            // "global" initialization here; Called before every test method.
+        }
+
+        public void Dispose()
+        {
+            // "global" teardown here; Called after every test method.
+        }
+
+
+        [Fact]
+        public void GuestEntrance()
+        {
+
+        }
+    }
+}

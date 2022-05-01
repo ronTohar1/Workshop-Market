9375ae2817bfde3d791cc1a2bfbcd87fb3370822
Roi Kant <kant@post.bgu.ac.il>
2022-05-01 14:15:05 +0300

    a bit on admin facade and fixes

--- Server/MarketBackend/MarketBackend/ServiceLayer/SystemOperator.cs
+++ Server/MarketBackend/MarketBackend/ServiceLayer/SystemOperator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarketBackend.ServiceLayer.ServiceDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace MarketBackend.ServiceLayer
 {
     internal class SystemOperator
     {
+        public SystemOperator()
+        {
+
+        }
+
+        public Response<bool> OpenMarket()
+        {
+            return new Response<bool>();
+        }
     }
 }

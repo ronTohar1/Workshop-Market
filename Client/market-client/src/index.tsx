6a747aa77f38db2f96297613b1213b2a9d0c43f2
Ron Tohar <rontoh@post.bgu.ac.il>
2022-05-15 21:56:06 +0300

    Fixed some routings

--- Client/market-client/src/index.tsx
+++ Client/market-client/src/index.tsx
@@ -5,14 +5,24 @@ import App from './App';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <SignIn />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<SignUp />} />
+      <Route path="register" element={<SignUp />} />
+      <Route path="login" element={<SignIn />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

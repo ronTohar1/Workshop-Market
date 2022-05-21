f413b56fcaff7b16e556907d718d9a3ce52c9c6e
Nir Barel <banir@post.bgu.ac.il>
2022-05-21 18:25:54 +0300

    add filtering by query

--- Client/market-client/src/App.tsx
+++ Client/market-client/src/App.tsx
@@ -1,36 +1,33 @@
-import React from 'react';
-import { useState, useEffect } from 'react'
-import './App.css';
-import Product from './DTOs/Product';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Product from "./DTOs/Product";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className='App'>
+      <header className='App-header'></header>
     </div>
   );
-}
-
+};
 
 // Fetch Products
 // should be async
-export const fetchProducts = (query : string) => {
+export const fetchProducts = (query: string) => {
   // const res = await fetch('http://localhost:5000/products/query=...')
   // const data = await res.json()
 
   return [
-    new Product(1, "apple", 2.4, "fruits"), 
-    new Product(2, "keyboard", 199, "elecronics"), 
-    new Product(3, "banana", 3.5, "fruits"), 
-    new Product(4, "pineapple", 9, "fruits"), 
-    new Product(5, "xiaomi 9", 250, "cellphones"), 
-    new Product(6, "xiaomi 10", 300, "cellphones"), 
-    new Product(7, "milk", 1.5, "dairy"), 
-    new Product(8, "butter", 1.8, "dairy"), 
-    new Product(9, "cheese", 1.8, "dairy"), 
-  ]
-}
-
+    new Product(1, "apple", 2.4, "fruits"),
+    new Product(2, "keyboard", 199, "elecronics"),
+    new Product(3, "banana", 3.5, "fruits"),
+    new Product(4, "pineapple", 9, "fruits"),
+    new Product(5, "xiaomi 9", 250, "cellphones"),
+    new Product(6, "xiaomi 10", 300, "cellphones"),
+    new Product(7, "milk", 1.5, "dairy"),
+    new Product(8, "butter", 1.8, "dairy"),
+    new Product(9, "cheese", 1.8, "dairy"),
+  ].filter((p) => p.name.includes(query));
+};
 
 export default App;

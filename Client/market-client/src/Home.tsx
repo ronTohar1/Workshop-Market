1238032759e9310ab2b159f0a9d9d089a5e5fc4f
davidsr4321 <davidsin@post.bgu.ac.il>
2022-05-22 19:36:47 +0300

    Update Home.tsx

--- Client/market-client/src/Home.tsx
+++ Client/market-client/src/Home.tsx
@@ -7,16 +7,29 @@ import Box from '@mui/material/Box';
 import {createTheme, ThemeProvider } from '@mui/material/styles';
 import Navbar from './Navbar';
 import {pathLogin, pathRegister} from './Paths'
-
-
+import {Typography}  from '@mui/material';
+import ProductHeroLayout from './ProductHeroLayout';
+import { styled } from '@mui/material/styles';
 
 const cards = [1, 2, 3];
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1471193945509-9ad0617afabf?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170';
+  
 
 const theme = createTheme();
 
 const createButton = (name: string, path: string) => {
   return <Button href={path} style={{height: 100, width: 500}}                  
-                  key="name" size='large' color='primary' >{name}</Button> 
+                  key="name" variant="contained" size='large' color='primary' >{name}</Button> 
+                //   <Button
+                //   color="secondary"
+                //   variant="contained"
+                //   size="large"
+                //   component="a"
+                //   href="/store"
+                //   sx={{ minWidth: 200 }}
+                // >
 };
 const buttons = [
   createButton("Continue To Website", pathLogin),
@@ -28,17 +41,38 @@ export default function Home() {
   const bar = Navbar();
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position="relative">
-      </AppBar>
       <main>
-        <Grid container
-              spacing={0}
-              direction="column"
-              alignItems="center"
-              justifyContent="center"
-              style={{ minHeight: '100vh' }}
-          >
-          <Box sx={{
+    
+            <ProductHeroLayout
+      sxBackground={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: '#7fc7d9', // Average color of the background image.
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: 'none' }}
+        src={backgroundImage}
+        alt="increase priority"
+      />
+      <Typography 
+      color="primary"
+      align="center"
+      variant="h2">
+       
+        Buy smart and cheap
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
+      >
+        Enjoy a variety of products, and mostly the suffering of 6 naive students
+      </Typography>
+      <Box sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
@@ -52,8 +86,12 @@ export default function Home() {
                 {buttons}
               </ButtonGroup>
             </Box>
-        </Grid>
-          
+      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+        Discover the experience
+      </Typography>
+    </ProductHeroLayout>
+  
+      
 
         {/* <Box
           sx={{
